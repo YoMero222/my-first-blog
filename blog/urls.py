577b329b2cdade7504836cd88cb0ae322eee399e
YoMero222 <obustamanc@gmail.com>
2020-06-26 23:30:07 -0500

    noticias y galeria

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('noticias/<int:pk>/', views.post_detail, name='post_detail'),
+    path('noticias/no/<int:num>/', views.noticias, name='noticias'),
+    path('galerias/<int:num>/', views.galerias, name='galerias'),
+    path('galeria/<int:pk>/', views.galeria, name='galeria'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,63 @@ from .models import Galeria
 from .models import ImagenEnGaleria
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-
+import math
+from array import array
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:3]
     integrantes= Integrantes.objects.all
-    galerias=Galeria.objects.all
+    galerias=Galeria.objects.order_by('-pk')[0:3]
+
+
 
     return render(request, 'blog/post_list.html', {"posts":posts, "integrantes":integrantes,"galerias":galerias})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def noticias(request, num):
+    n=int(num)
+    p=3
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[(n-1)*p:(n-1)*p+(p)]
+    total = Post.objects.count()
+    ultima=math.ceil(total/p)
+    total_array=[]
+    print(ultima)
+    if num-3<=0:
+        for x in range(1,ultima+1):
+            total_array.append(x)
+    elif num+3>ultima:
+        for x in range(ultima-3,ultima+1):
+            total_array.append(x)
+    else:
+        for x in range(ultima-2,ultima+2):
+            total_array.append(x)
+    print(total_array)
+    return render(request, 'blog/noticias.html', {'posts':posts,'num':num,'total_array':total_array })
+
+def galerias(request, num):
+    n=int(num)
+    p=3
+    galerias = Galeria.objects.order_by('-pk')[(n-1)*p:(n-1)*p+(p)]
+    total = Galeria.objects.count()
+    ultima=math.ceil(total/p)
+    total_array=[]
+
+    if num-3<=0:
+        for x in range(1,ultima+1):
+            total_array.append(x)
+    elif num+3>ultima:
+        for x in range(ultima-3,ultima+1):
+            total_array.append(x)
+    else:
+        for x in range(ultima-2,ultima+2):
+            total_array.append(x)
+    return render(request, 'blog/galerias.html', {'galerias':galerias,'num':num,'total_array':total_array })
+
+def galeria(request,pk):
+    imagenes=ImagenEnGaleria.objects.filter(galeria=pk)
+    nombre=Galeria.objects.filter(pk=pk)
+    return render(request, 'blog/galeria.html',{'imagenes':imagenes,'nombre':nombre})
